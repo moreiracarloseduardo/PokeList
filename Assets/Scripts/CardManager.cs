cfd33a95283938450bec04f5cc9bea00263fe632
Kadu <moreira.carloseduardo@gmail.com>
2023-11-12 01:04:36 -0300

    Add background color based on species color and store color name in Pokemon object

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -60,7 +60,22 @@ public class CardManager : MonoBehaviour
         nameText.text = pokemon.name;
         weightText.text = $"Weight: {pokemon.weight}";
         orderText.text = $"Order: {pokemon.order}";
+
+        if (!string.IsNullOrWhiteSpace(pokemon.color))
+        {
+            // Set background color based on species color
+            backgroundImage.color = ConvertColor(pokemon.color);
+        }
+        else
+        {
+            Debug.LogWarning("Pokemon color is null or empty.");
+            // Decida o que fazer neste caso. Por exemplo, você pode usar uma cor padrão.
+            backgroundImage.color = Color.white; // ou qualquer outra cor padrão desejada
+        }
     }
+
+
+
 
     public IEnumerator LoadImage(string url, RawImage imageComponent)
     {
@@ -92,4 +107,40 @@ public class CardManager : MonoBehaviour
             imageComponent.texture = texture;
         }
     }
+    Color ConvertColor(string colorName)
+    {
+        if (string.IsNullOrEmpty(colorName))
+        {
+            Debug.LogWarning("Color name is null or empty.");
+            return Color.white; // ou qualquer outra cor padrão desejada
+        }
+
+        switch (colorName.ToLower())
+        {
+            case "black":
+                return new Color(0.4f, 0.4f, 0.4f); // Lighter black
+            case "blue":
+                return new Color(0.6f, 0.6f, 1.0f); // Pastel blue
+            case "brown":
+                return new Color(0.76f, 0.6f, 0.42f); // Pastel brown
+            case "gray":
+                return new Color(0.8f, 0.8f, 0.8f); // Lighter gray
+            case "green":
+                return new Color(0.6f, 1.0f, 0.6f); // Pastel green
+            case "pink":
+                return new Color(1.0f, 0.85f, 0.9f); // Pastel pink
+            case "purple":
+                return new Color(0.8f, 0.6f, 1.0f); // Pastel purple
+            case "red":
+                return new Color(1.0f, 0.6f, 0.6f); // Pastel red
+            case "white":
+                return Color.white;
+            case "yellow":
+                return new Color(1.0f, 1.0f, 0.6f); // Pastel yellow
+            default:
+                Debug.LogWarning("Unknown color name: " + colorName);
+                return Color.white; // Default color
+        }
+    }
+
 }
--- Assets/Scripts/PokemonAPIManager.cs
+++ Assets/Scripts/PokemonAPIManager.cs
@@ -12,8 +12,9 @@ public class Pokemon
     public int weight;
     public int order;
     public PokemonSprites sprites;
-    public Color color;
     public string url; // Add this field to store the URL
+    public string color = ""; // Change to string to store color name
+
 
     public int Id
     {
@@ -40,6 +41,12 @@ public class Pokemon
             }
         }
     }
+
+    public void SetColor(string newColor)
+    {
+        color = newColor;
+    }
+
 }
 
 [Serializable]
@@ -70,7 +77,10 @@ public class PokemonAPIManager : MonoBehaviour
             {
                 Pokemon pokemon = JsonUtility.FromJson<Pokemon>(www.downloadHandler.text);
                 pokemon.url = url; // Set the URL manually
-                StartCoroutine(GetPokemonSpecies(pokemon));
+
+                // Wait until GetPokemonSpecies has completed before adding the Pokemon to the list
+                yield return StartCoroutine(GetPokemonSpecies(pokemon));
+
                 pokemons.Add(pokemon);
             }
         }
@@ -89,7 +99,9 @@ public class PokemonAPIManager : MonoBehaviour
             {
                 string json = request.downloadHandler.text;
                 PokemonSpecies species = JsonUtility.FromJson<PokemonSpecies>(json);
-                cardManager.backgroundImage.color = ConvertColor(species.color.name); // Set the background color
+
+                // Set the color directly in the Pokemon object
+                pokemon.SetColor(species.color.name);
             }
             else
             {
@@ -101,33 +113,5 @@ public class PokemonAPIManager : MonoBehaviour
             Debug.LogError("Pokemon object is null.");
         }
     }
-    Color ConvertColor(string colorName)
-    {
-        switch (colorName.ToLower())
-        {
-            case "black":
-                return new Color(0.4f, 0.4f, 0.4f); // Lighter black
-            case "blue":
-                return new Color(0.6f, 0.6f, 1.0f); // Pastel blue
-            case "brown":
-                return new Color(0.76f, 0.6f, 0.42f); // Pastel brown
-            case "gray":
-                return new Color(0.8f, 0.8f, 0.8f); // Lighter gray
-            case "green":
-                return new Color(0.6f, 1.0f, 0.6f); // Pastel green
-            case "pink":
-                return new Color(1.0f, 0.85f, 0.9f); // Pastel pink
-            case "purple":
-                return new Color(0.8f, 0.6f, 1.0f); // Pastel purple
-            case "red":
-                return new Color(1.0f, 0.6f, 0.6f); // Pastel red
-            case "white":
-                return Color.white;
-            case "yellow":
-                return new Color(1.0f, 1.0f, 0.6f); // Pastel yellow
-            default:
-                Debug.LogWarning("Unknown color name: " + colorName);
-                return Color.white; // Default color
-        }
-    }
+
 }
